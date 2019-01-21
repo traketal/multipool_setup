2548cef7fc2899876fade583d0e9e79248b4f59a
traketal <alanmritchie@gmail.com>
2019-01-20 20:12:46 -0700

    Update bootstrap_coin.sh

--- bootstrap_coin.sh
+++ bootstrap_coin.sh
@@ -12,9 +12,7 @@ fi
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/multipool/daemon_builder ]; then
 	echo Downloading MultiPool Daemon Builder Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/cryptopool-builders/multipool_coin_builder \
+	git clone https://github.com/traketal/multipool_coin_builder \
 		$HOME/multipool/daemon_builder \
 		< /dev/null 2> /dev/null
 
@@ -23,18 +21,6 @@ fi
 
 # Change directory to it.
 cd $HOME/multipool/daemon_builder
-
-# Update it.
-sudo chown -R $USER $HOME/multipool/install/.git/
-if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating Daemon Builder Installer to ${TAG} . . .
-	git fetch --depth 1 --force --prune origin tag ${TAG}
-	if ! git checkout -q ${TAG}; then
-		echo "Update failed. Did you modify something in `pwd`?"
-		exit
-	fi
-	echo
-fi
 
 # Start setup script.
 cd $HOME/multipool/daemon_builder

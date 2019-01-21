4c785c05f386852e6d2ba34a07f999ddcaa1a09b
traketal <alanmritchie@gmail.com>
2019-01-21 13:12:33 -0700

    Update bootstrap_single.sh

--- bootstrap_single.sh
+++ bootstrap_single.sh
@@ -14,9 +14,7 @@ fi
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/multipool/yiimp_single ]; then
 	echo Downloading MultiPool YiiMP Single Server Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/cryptopool-builders/multipool_yiimp_single \
+	git clone https://github.com/traketal/multipool_yiimp_single \
 		$HOME/multipool/yiimp_single \
 		< /dev/null 2> /dev/null
 
